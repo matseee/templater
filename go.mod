0d3363217ac1d35977b5a6624b711cfd8f46e9c6
Matthias Fischer <m@tthias.space>
2022-07-25 20:21:53 +0200

    add keylogger example

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/matseee/templater
 
 go 1.18
 
-require github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
+require (
+	github.com/MarinX/keylogger v0.0.0-20210528193429-a54d7834cc1a
+	github.com/sirupsen/logrus v1.9.0
+)
 
 require golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
--- main.go
+++ main.go
@@ -1,27 +1,62 @@
 package main
 
 import (
-	"fmt"
+	"time"
 
-	"github.com/eiannone/keyboard"
+	"github.com/MarinX/keylogger"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	if err := keyboard.Open(); err != nil {
-		panic(err)
+
+	// find keyboard device, does not require a root permission
+	keyboard := keylogger.FindKeyboardDevice()
+
+	// check if we found a path to keyboard
+	if len(keyboard) <= 0 {
+		logrus.Error("No keyboard found...you will need to provide manual input path")
+		return
 	}
-	defer func() {
-		_ = keyboard.Close()
+
+	logrus.Println("Found a keyboard at", keyboard)
+	// init keylogger with keyboard
+	k, err := keylogger.New(keyboard)
+	if err != nil {
+		logrus.Error(err)
+		return
+	}
+	defer k.Close()
+
+	// write to keyboard example:
+	go func() {
+		time.Sleep(5 * time.Second)
+		// open text editor and focus on it, it should say "marin" and new line will be printed
+		keys := []string{"m", "a", "r", "i", "n", "ENTER"}
+		for _, key := range keys {
+			// write once will simulate keyboard press/release, for long press or release, lookup at Write
+			k.WriteOnce(key)
+		}
 	}()
 
-	fmt.Println("Press ESC to quit")
-	for {
-		char, key, err := keyboard.GetKey()
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("You pressed: rune %q, key %X\r\n", char, key)
-		if key == keyboard.KeyEsc {
+	events := k.Read()
+
+	// range of events
+	for e := range events {
+		switch e.Type {
+		// EvKey is used to describe state changes of keyboards, buttons, or other key-like devices.
+		// check the input_event.go for more events
+		case keylogger.EvKey:
+
+			// if the state of key is pressed
+			if e.KeyPress() {
+				logrus.Println("[event] press key ", e.KeyString())
+			}
+
+			// if the state of key is released
+			if e.KeyRelease() {
+				logrus.Println("[event] release key ", e.KeyString())
+			}
+
 			break
 		}
 	}
