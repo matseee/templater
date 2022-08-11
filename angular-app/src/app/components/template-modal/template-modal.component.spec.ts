7fbc02cfe0ea933f7cc6686b9bcb96107d14c4d5
Matthias Fischer <m@tthias.space>
2022-08-12 01:30:38 +0200

    feat(template-modal): add template-modal logic

--- angular-app/src/app/components/template-modal/template-modal.component.spec.ts
+++ angular-app/src/app/components/template-modal/template-modal.component.spec.ts
@@ -1,15 +1,73 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TemplateFacade } from './../../facades/template.facade';
+import { MaterialModule } from './../../material.module';
+import { Template } from './../../models/template.model';
 
 import { TemplateModalComponent } from './template-modal.component';
 
+class TemplateFacadeMock {
+  template: Template;
+
+  constructor() {
+    this.template = {
+      id: 'DUMMY',
+      name: 'DUMMY',
+      template: 'DUMMY',
+    };
+  }
+
+  createTemplate(template: Template) {
+    this.template = template;
+    this.template.id = 'Test123';
+  }
+
+  updateTemplate(template: Template) {
+    this.template = template;
+  }
+}
+
+class MatDialogRefMock {
+  isClosed: boolean = false;
+  constructor() { }
+
+  reset() {
+    this.isClosed = false;
+  }
+
+  close() {
+    this.isClosed = true;
+  }
+}
+
 describe('TemplateModalComponent', () => {
+  let templateFacade: TemplateFacadeMock;
+  let matDialogRef: MatDialogRefMock;
   let component: TemplateModalComponent;
   let fixture: ComponentFixture<TemplateModalComponent>;
   let compiled: any;
 
   beforeEach(async () => {
+    templateFacade = new TemplateFacadeMock();
+    matDialogRef = new MatDialogRefMock();
+
     await TestBed.configureTestingModule({
-      declarations: [TemplateModalComponent]
+      imports: [
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MaterialModule,
+      ],
+      declarations: [TemplateModalComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: matDialogRef },
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: TemplateFacade, useValue: templateFacade }
+      ]
     })
       .compileComponents();
 
@@ -24,15 +82,18 @@ describe('TemplateModalComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have a title', () => {
-    component.templateName = 'Test123';
-    fixture.detectChanges();
+  it('should be a new creation', () => {
+    expect(component).toBeTruthy();
+    expect(component.isEditMode).toBeFalse();
+  });
 
-    expect(component.templateName).toBe('Test123');
+  it('should have the title "New template"', () => {
+    fixture.detectChanges();
+    expect(component.templateName).toBe('');
 
     const title = compiled.querySelector('.title');
     expect(title).toBeTruthy();
-    expect(title.innerHTML).toBe('Template Test123');
+    expect(title.innerHTML).toBe('New template');
   });
 
   it('should have a form-group', () => {
@@ -59,4 +120,172 @@ describe('TemplateModalComponent', () => {
     fixture.detectChanges();
     expect(compiled.querySelector('#button-cancle')).toBeTruthy();
   });
+
+  it('should only enable the save button when the formgroup is valid', () => {
+    fixture.detectChanges();
+    expect(compiled.querySelector('#button-save').disabled).toBeTrue();
+
+    component.formGroup.setValue({
+      name: 'Test123',
+      template: 'Test123'
+    });
+
+    fixture.detectChanges();
+    expect(compiled.querySelector('#button-save').disabled).toBeFalse();
+  });
+
+  it('should create the template after clicking on button save', async () => {
+    matDialogRef.reset();
+
+    fixture.detectChanges();
+
+    component.formGroup.patchValue({
+      name: 'Test123',
+      template: 'Test1234'
+    });
+
+    fixture.detectChanges();
+    expect(compiled.querySelector('#button-save').disabled).toBeFalse();
+
+    const buttonSave = fixture.debugElement.query(By.css('#button-save'));
+    buttonSave.triggerEventHandler('click', null);
+
+    fixture.whenStable().then(() => {
+      expect(templateFacade.template.id).toBe('Test123');
+      expect(templateFacade.template.name).toBe('Test123');
+      expect(templateFacade.template.template).toBe('Test1234');
+      expect(matDialogRef.isClosed).toBe(true);
+    });
+  });
+
+  it('should close the modal after clicking on button cancle', async () => {
+    matDialogRef.reset();
+
+    fixture.detectChanges();
+
+    const buttonCancle = fixture.debugElement.query(By.css('#button-cancle'));
+    buttonCancle.triggerEventHandler('click', null);
+
+    fixture.whenStable().then(() => {
+      expect(matDialogRef.isClosed).toBe(true);
+    });
+  });
 });
+
+describe('TemplateModalComponent-WithData', () => {
+  let templateFacade: TemplateFacadeMock;
+  let matDialogRef: MatDialogRefMock;
+  let component: TemplateModalComponent;
+  let fixture: ComponentFixture<TemplateModalComponent>;
+  let compiled: any;
+
+  beforeEach(async () => {
+    templateFacade = new TemplateFacadeMock();
+    matDialogRef = new MatDialogRefMock();
+
+    await TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        MaterialModule,
+      ],
+      declarations: [TemplateModalComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: matDialogRef },
+        {
+          provide: MAT_DIALOG_DATA, useValue: {
+            data: {
+              id: 'test123',
+              name: 'test123',
+              template: 'test123',
+            }
+          }
+        },
+        { provide: TemplateFacade, useValue: templateFacade }
+      ]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(TemplateModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    compiled = fixture.debugElement.nativeElement;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have a title', () => {
+    fixture.detectChanges();
+
+    expect(component.templateName).toBe('test123');
+
+    const title = compiled.querySelector('.title');
+    expect(title).toBeTruthy();
+    expect(title.innerHTML).toBe('Template test123');
+  });
+
+  it('should have a form-group', () => {
+    fixture.detectChanges();
+    expect(compiled.querySelector('form.form-group')).toBeTruthy();
+  });
+
+  it('should contain the input "name" with the value "test123"', () => {
+    fixture.detectChanges();
+    const input = compiled.querySelector('#input-name');
+    expect(input).toBeTruthy();
+    expect(input.value).toBe('test123');
+  });
+
+  it('should contain the textarea "template" with the vlaue "test123"', () => {
+    fixture.detectChanges();
+    const input = compiled.querySelector('#textarea-template');
+    expect(input).toBeTruthy();
+    expect(input.value).toBe('test123');
+  });
+
+  it('should contain the button save', () => {
+    fixture.detectChanges();
+    expect(compiled.querySelector('#button-save')).toBeTruthy();
+  });
+
+  it('should contain the button cancle', () => {
+    fixture.detectChanges();
+    expect(compiled.querySelector('#button-cancle')).toBeTruthy();
+  });
+
+  it('should update the template after clicking on button save', async () => {
+    matDialogRef.reset();
+
+    fixture.detectChanges();
+
+    component.formGroup.patchValue({
+      template: 'edited',
+    });
+
+    const buttonSave = fixture.debugElement.query(By.css('#button-save'));
+    buttonSave.triggerEventHandler('click', null);
+
+    fixture.whenStable().then(() => {
+      expect(templateFacade.template.template).toBe('edited');
+      expect(matDialogRef.isClosed).toBe(true);
+    });
+  });
+
+  it('should close the modal after clicking on button cancle', async () => {
+    matDialogRef.reset();
+
+    fixture.detectChanges();
+
+    const buttonCancle = fixture.debugElement.query(By.css('#button-cancle'));
+    buttonCancle.triggerEventHandler('click', null);
+
+    fixture.whenStable().then(() => {
+      expect(matDialogRef.isClosed).toBe(true);
+    });
+  });
+});
+
--- angular-app/src/app/components/template-modal/template-modal.component.ts
+++ angular-app/src/app/components/template-modal/template-modal.component.ts
@@ -1,17 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { TemplateFacade } from './../../facades/template.facade';
+import { Template } from './../../models/template.model';
+
+export interface TemplateModalData {
+  data: Template;
+}
 
 @Component({
   selector: 'app-template-modal',
   templateUrl: './template-modal.component.html',
   styleUrls: ['./template-modal.component.scss']
 })
-export class TemplateModalComponent implements OnInit {
+export class TemplateModalComponent {
+  isEditMode: boolean = false;
 
-  public templateName: string = '';
+  templateName: string = '';
+  formGroup: FormGroup;
 
-  constructor() { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public myTemplate: TemplateModalData,
+    public myDialogRef: MatDialogRef<TemplateModalComponent>,
+    public myFormBuilder: FormBuilder,
+    public myTemplateFacade: TemplateFacade,
+  ) {
+    this.formGroup = this.myFormBuilder.group({
+      'name': ['', Validators.required],
+      'template': ['', Validators.required]
+    });
 
-  ngOnInit(): void {
+    if (myTemplate && myTemplate.data) {
+      this.isEditMode = true;
+      this.templateName = myTemplate.data.name;
+
+      this.formGroup.setValue({
+        name: myTemplate.data.name,
+        template: myTemplate.data.template,
+      });
+    }
   }
 
+  onCancle() {
+    this.myDialogRef.close();
+  }
+
+  onSave() {
+    const formGroupValue = this.formGroup.getRawValue();
+    const template: Template = {
+      id: this.myTemplate && this.myTemplate.data ? this.myTemplate.data.id : '',
+      name: formGroupValue.name,
+      template: formGroupValue.template,
+    };
+
+    if (this.isEditMode) {
+      this.myTemplateFacade.updateTemplate(template);
+    } else {
+      this.myTemplateFacade.createTemplate(template);
+    }
+
+    this.myDialogRef.close();
+  }
 }
