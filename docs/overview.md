68adaffb9519d0e15e294e1f706391d8474ef2e0
Matthias Fischer <matthias.fischer@sla.de>
2022-08-28 20:30:17 +0200

    refactoring application

---
+++ docs/overview.md
@@ -0,0 +1,10 @@
+# Documentation
+The functionality of the application is written in golang and the frontend is written in angular.
+
+## Module overview
+Templater is based on the following modules:
+| module      | description                                                       |
+| ----------- | ----------------------------------------------------------------- |
+| templater   |                                                                   |
+| keylistener | listens to the keyboard and sends events to the templater package |
+| gui         | creates systemtray and webview for the gui                        |
--- keylistener/keylogger_linux.go
+++ keylistener/keylogger_linux.go
@@ -3,16 +3,57 @@ package keylistener
 import "github.com/MarinX/keylogger"
 
 type KeyloggerLinux struct {
-	outChannel  chan KeyEvent
-	isCreated   bool
-	isListening bool
+	outChannel chan KeyEvent
+	status     KeyloggerStatus
 
-	// specfic attributes
 	keylogger *keylogger.KeyLogger
 	inChannel chan keylogger.InputEvent
 }
 
-func (k *KeyloggerLinux) Create() error {
+func (k *KeyloggerLinux) Start() (chan KeyEvent, error) {
+	if k.status.IsListening {
+		return k.outChannel, nil
+	}
+
+	if !k.status.IsCreated {
+		if err := k.create(); err != nil {
+			return nil, err
+		}
+	}
+
+	k.outChannel = make(chan KeyEvent)
+	k.status.IsListening = true
+	return k.outChannel, nil
+}
+
+func (k *KeyloggerLinux) Stop() {
+	if !k.status.IsListening {
+		return
+	}
+
+	close(k.outChannel)
+	k.outChannel = nil
+
+	k.status.IsListening = false
+}
+
+func (k *KeyloggerLinux) Destroy() error {
+	if !k.status.IsCreated {
+		return nil
+	}
+
+	k.keylogger.Close()
+
+	k.status.IsListening = false
+	k.status.IsCreated = false
+	return nil
+}
+
+func (k *KeyloggerLinux) GetStatus() KeyloggerStatus {
+	return k.status
+}
+
+func (k *KeyloggerLinux) create() error {
 	var err error
 
 	k.keylogger, err = keylogger.New("/dev/input/event19")
@@ -24,55 +65,13 @@ func (k *KeyloggerLinux) Create() error {
 	k.inChannel = k.keylogger.Read()
 	go k.listen()
 
-	k.isCreated = true
+	k.status.IsCreated = true
 	return nil
-}
-
-func (k *KeyloggerLinux) Destroy() error {
-	if !k.isCreated {
-		return nil
-	}
-
-	k.keylogger.Close()
-
-	k.isListening = false
-	k.isCreated = false
-	return nil
-}
-
-func (k *KeyloggerLinux) Start() chan KeyEvent {
-	if k.isListening {
-		return k.outChannel
-	}
-
-	k.outChannel = make(chan KeyEvent)
-
-	k.isListening = true
-	return k.outChannel
-}
-
-func (k *KeyloggerLinux) Stop() {
-	if !k.isListening {
-		return
-	}
-
-	close(k.outChannel)
-	k.outChannel = nil
-
-	k.isListening = false
-}
-
-func (k *KeyloggerLinux) IsCreated() bool {
-	return k.isCreated
-}
-
-func (k *KeyloggerLinux) IsListening() bool {
-	return k.isListening
 }
 
 func (k *KeyloggerLinux) listen() {
 	for inputEvent := range k.inChannel {
-		if k.isListening {
+		if k.status.IsListening {
 			switch inputEvent.Type {
 			case keylogger.EvKey:
 				var outEvent KeyEvent
