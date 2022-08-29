67ce9ed093c507856b58afb23b30be072f762174
Matthias Fischer <m@tthias.space>
2022-08-30 01:11:39 +0200

    renamed Test_CreateEvent function

--- templater/event_test.go
+++ templater/event_test.go
@@ -2,12 +2,10 @@ package templater
 
 import "testing"
 
-func Test_CreateEvent(t *testing.T) {
+func Test_CreateEvent_should_return_a_empty_event_of_type_StatusEvent(t *testing.T) {
 	event := CreateEvent()
 
 	if event.Type != StatusEvent || event.ValueBool != false || event.ValueInt != 0 || event.ValueString != "" {
 		t.Error("CreateEvent should create an empty event - FAILED")
-	} else {
-		t.Log("CreateEvent created an empty event - PASSED")
 	}
 }
