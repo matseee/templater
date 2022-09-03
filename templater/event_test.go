d64a39308f8558cbe68c643eb8abdf7a04b72037
Matthias Fischer <m@tthias.space>
2022-09-03 11:12:23 +0200

    refactor event tests

--- templater/event_test.go
+++ templater/event_test.go
@@ -6,6 +6,6 @@ func Test_CreateEvent_should_return_a_empty_event_of_type_StatusEvent(t *testing
 	event := CreateEvent()
 
 	if event.Type != StatusEvent || event.ValueBool != false || event.ValueInt != 0 || event.ValueString != "" {
-		t.Error("CreateEvent should create an empty event - FAILED")
+		t.Error("CreateEvent should create an empty event")
 	}
 }
