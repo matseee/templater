41611587e066877ad3fd339d5ae699c9b6c30606
Matthias Fischer <m@tthias.space>
2022-09-12 20:21:11 +0200

    add gui object

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.18
 
 require (
 	github.com/MarinX/keylogger v0.0.0-20210528193429-a54d7834cc1a
-	github.com/gin-gonic/gin v1.8.1
 	github.com/sirupsen/logrus v1.9.0
 )
 
@@ -15,27 +14,9 @@ require (
 	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
 	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
 	github.com/getlantern/ops v0.0.0-20190325191751-d70cb0d6f85f // indirect
-	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-playground/locales v0.14.0 // indirect
-	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
-	github.com/goccy/go-json v0.9.10 // indirect
-	github.com/google/go-cmp v0.5.6 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
-	github.com/pelletier/go-toml/v2 v2.0.2 // indirect
-	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
-	golang.org/x/net v0.0.0-20220802222814-0bcc04d9c69b // indirect
-	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/stretchr/testify v1.7.2 // indirect
 )
 
 require (
--- gui/gui.go
+++ gui/gui.go
@@ -1,55 +1,70 @@
 package gui
 
 import (
-	"math/rand"
-	"net"
-	"strconv"
-	"time"
-
-	"github.com/gin-gonic/gin"
 	"github.com/matseee/templater/templater"
 )
 
-const (
-	PORTMIN, PORTMAX int = 49152, 65535
-)
-
-var (
-	port int
-)
-
-func InitGUI(channel chan templater.Event) error {
-	port = getRandomPort()
-
-	for !isPortAvailable(port) {
-		port = getRandomPort()
-	}
-
-	return createRouter(port)
+func CreateGui(eventChannel templater.EventChannel) Gui {
+	gui := *new(Gui)
+	gui.eventChannel = eventChannel
+	return gui
 }
 
-func getRandomPort() int {
-	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(PORTMAX-PORTMIN) + PORTMIN
+type Gui struct {
+	eventChannel templater.EventChannel
+
+	systemtray Systemtray
+	api        Api
 }
 
-func getURL() string {
-	return "http://127.0.0.1:" + strconv.Itoa(port) + "/"
+func (g *Gui) SetSystemtray(systemtray Systemtray) {
+	g.systemtray = systemtray
 }
 
-func isPortAvailable(port int) bool {
-	l, err := net.Listen("tcp", ":"+strconv.Itoa(port))
-
-	if err != nil {
-		return false
-	}
-
-	l.Close()
-	return true
+func (g *Gui) SetApi(api Api) {
+	g.api = api
 }
 
-func createRouter(port int) error {
-	router := gin.Default()
-	router.Static("/", "./frontend")
-	return router.Run(":" + strconv.Itoa(port))
-}
+// const (
+// 	PORTMIN, PORTMAX int = 49152, 65535
+// )
+
+// var (
+// 	port int
+// )
+
+// func InitGUI(channel chan templater.Event) error {
+// 	port = getRandomPort()
+
+// 	for !isPortAvailable(port) {
+// 		port = getRandomPort()
+// 	}
+
+// 	return createRouter(port)
+// }
+
+// func getRandomPort() int {
+// 	rand.Seed(time.Now().UnixNano())
+// 	return rand.Intn(PORTMAX-PORTMIN) + PORTMIN
+// }
+
+// func getURL() string {
+// 	return "http://127.0.0.1:" + strconv.Itoa(port) + "/"
+// }
+
+// func isPortAvailable(port int) bool {
+// 	l, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+
+// 	if err != nil {
+// 		return false
+// 	}
+
+// 	l.Close()
+// 	return true
+// }
+
+// func createRouter(port int) error {
+// 	router := gin.Default()
+// 	router.Static("/", "./frontend")
+// 	return router.Run(":" + strconv.Itoa(port))
+// }
