c208c30ea80677ee8b7f43a1c6408003d336e31c
Matthias Fischer <m@tthias.space>
2022-09-14 01:23:28 +0200

    removed old code from gui

--- gui/api.go
+++ gui/api.go
@@ -4,3 +4,47 @@ type Api interface {
 	Run()
 	Quit()
 }
+
+// const (
+// 	PORTMIN, PORTMAX int = 49152, 65535
+// )
+
+// var (
+// 	port int
+// )
+
+// func InitGUI(channel chan templater.Event) error {
+// 	port = getRandomPort()
+
+// 	for !isPortAvailable(port) {
+// 		port = getRandomPort()
+// 	}
+
+// 	return createRouter(port)
+// }
+
+// func getRandomPort() int {
+// 	rand.Seed(time.Now().UnixNano())
+// 	return rand.Intn(PORTMAX-PORTMIN) + PORTMIN
+// }
+
+// func getURL() string {
+// 	return "http://127.0.0.1:" + strconv.Itoa(port) + "/"
+// }
+
+// func isPortAvailable(port int) bool {
+// 	l, err := net.Listen("tcp", ":"+strconv.Itoa(port))
+
+// 	if err != nil {
+// 		return false
+// 	}
+
+// 	l.Close()
+// 	return true
+// }
+
+// func createRouter(port int) error {
+// 	router := gin.Default()
+// 	router.Static("/", "./frontend")
+// 	return router.Run(":" + strconv.Itoa(port))
+// }
